d5dcca932c5ca9c8feb90fdd2a64c568ea91dece
Tejaswiniprabhakaran <tejaswiniprabhakarann@gmail.com>
2026-05-14 19:32:00 +0530

    Fix Render static serving: add STATICFILES_DIRS, use CompressedStaticFilesStorage, clean collectstatic build

--- Flat_Rent_django/Flat_Rent_django/settings.py
+++ Flat_Rent_django/Flat_Rent_django/settings.py
@@ -124,7 +124,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'Flatapp', 'static')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+WHITENOISE_MANIFEST_STRICT = False
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
